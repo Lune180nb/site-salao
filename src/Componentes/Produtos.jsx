22b71d47f4fbb141ba11f26be5364593e95b9dba
Lune Souza <lunesouza2@gmail.com>
2026-07-28 23:46:11 -0300

    atualizacao do site

--- src/Componentes/Produtos.jsx
+++ src/Componentes/Produtos.jsx
@@ -37,28 +37,30 @@ function Produtos({mostrarEsquerda,mostrarDireita,index}){
       </div>
       <div ref = {cardRef3} className = "produtos-servicos baixo">
       <button onClick = { mostrarEsquerda} className = "esquerda">&#10094;</button>
-        <div className = "slides">
-          <div className = "cards" style = {{transform:`translateX(-${index * 100}%)`}}>
+      
+      <div className = "janela">
+        <div className = "slides" style = {{transform:`translateX(-${index * 100}%)`}}>
+          <div className = "cards">
             <img src = {servico} alt = "imagem de uma pessoa fazendo manicure"/>
             <h2>Manicure & Pedicure</h2>
             <p>Cuidados completos para mãos e pés, com acabamento impecável, higiene e atenção aos detalhes.</p>
             <a href = "#" className = "link-whatsapp">Agenda aqui</a>
           </div>
-            <div className = "cards" style = {{transform:`translateX(-${index * 100}%)`}}>
+            <div className = "cards">
             <img src = {servico2} alt = "imagem de uma pessoa fazendo coloração de uma cliente"/>
             <h2>Coloração</h2>
             <p>
 Transforme o visual com técnicas de coloração, luzes e retoques realizados com produtos de alta qualidade.</p>
             <a href = "#" className = "link-whatsapp">Agenda aqui</a>
           </div>
-            <div className = "cards" style = {{transform:`translateX(-${index * 100}%)`}}>
+            <div className = "cards">
             <img src = {servico3} alt = "imagem de uma pessoa cortando cabelo"/>
             <h2>Corte & Finalização</h2>
             <p>
 Realce seu estilo com cortes modernos e finalizações feitas para valorizar a sua beleza e personalidade.</p>
             <a href = "#" className = "link-whatsapp">Agenda aqui</a>
           </div>
-                    <div className = "cards" style = {{transform:`translateX(-${index * 100}%)`}}>
+                    <div className = "cards">
             <img src = {servico4} alt = "imagem de uma pessoa fazendo exague no cabelo de uma cliente"/>
             <h2>Tratamentos Capilares</h2>
             <p>
@@ -66,6 +68,7 @@ Recupere a saúde dos fios com hidratação, nutrição e reconstrução para ca
             <a href = "#" className = "link-whatsapp">Agenda aqui</a>
           </div>
           </div>
+      </div>
       <button onClick = {mostrarDireita} className = "direita">&#10095;</button>
       </div>
     </div>
